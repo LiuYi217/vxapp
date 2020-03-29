ed7d339b041ee05793b9ca6b55a30008fdc1389a
刘毅 <liuyiab@chanjet.com>
2020-03-29 22:33:36 +0800

    feat: 项目架构搭建

---
+++ config/base_setting.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# encoding: utf-8
+SERVER_PORT = 5000
+DEBUG = False
+SQLALCHEMY_ECHO = False
---
+++ www.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# encoding: utf-8
+"""
+作者:糖葫芦
+创建时间:2020/3/2919:06
+文件:www.py
+IDE:PyCharm
+"""
